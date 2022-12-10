d499729a7dea2dce613d3b9fb1945abaf285d7aa
EdgarVeider <guimatos.com@gmail.com>
2022-12-10 14:44:46 -0300

    LS Funcionando

--- function_ext2.c
+++ function_ext2.c
@@ -17,8 +17,8 @@ static unsigned int block_size = 0;        /* block size (to be calculated) */
 static void read_dir(int, const struct ext2_inode*, const struct ext2_group_desc*);
 static void read_inode(int, int, const struct ext2_group_desc*, struct ext2_inode*);
 
-void Read_SuperBlock(){
-		struct ext2_super_block super;
+struct ext2_super_block Read_SuperBlock(int print){
+	struct ext2_super_block super;
 	int fd;
 
 	/* open floppy device */
@@ -41,36 +41,39 @@ void Read_SuperBlock(){
 		
 	block_size = 1024 << super.s_log_block_size;
 
-	printf("Reading super-block from device " FD_DEVICE ":\n"
-	       "Inodes count            : %u\n"
-	       "Blocks count            : %u\n"
-	       "Reserved blocks count   : %u\n"
-	       "Free blocks count       : %u\n"
-	       "Free inodes count       : %u\n"
-	       "First data block        : %u\n"
-	       "Block size              : %u\n"
-	       "Blocks per group        : %u\n"
-	       "Inodes per group        : %u\n"
-	       "Creator OS              : %u\n"
-	       "First non-reserved inode: %u\n"
-	       "Size of inode structure : %hu\n"
-	       ,
-	       super.s_inodes_count,  
-	       super.s_blocks_count,
-	       super.s_r_blocks_count,     /* reserved blocks count */
-	       super.s_free_blocks_count,
-	       super.s_free_inodes_count,
-	       super.s_first_data_block,
-	       block_size,
-	       super.s_blocks_per_group,
-	       super.s_inodes_per_group,
-	       super.s_creator_os,
-	       super.s_first_ino,          /* first non-reserved inode */
-	       super.s_inode_size);
+	if(print == 1){
+		printf("Reading super-block from device " FD_DEVICE ":\n"
+			"Inodes count            : %u\n"
+			"Blocks count            : %u\n"
+			"Reserved blocks count   : %u\n"
+			"Free blocks count       : %u\n"
+			"Free inodes count       : %u\n"
+			"First data block        : %u\n"
+			"Block size              : %u\n"
+			"Blocks per group        : %u\n"
+			"Inodes per group        : %u\n"
+			"Creator OS              : %u\n"
+			"First non-reserved inode: %u\n"
+			"Size of inode structure : %hu\n"
+			,
+			super.s_inodes_count,  
+			super.s_blocks_count,
+			super.s_r_blocks_count,     /* reserved blocks count */
+			super.s_free_blocks_count,
+			super.s_free_inodes_count,
+			super.s_first_data_block,
+			block_size,
+			super.s_blocks_per_group,
+			super.s_inodes_per_group,
+			super.s_creator_os,
+			super.s_first_ino,          /* first non-reserved inode */
+			super.s_inode_size);
+	}
 	
+	return super;
 }
 
-void Read_GroupDesc(){
+struct ext2_group_desc Read_GroupDesc(int print){
 	struct ext2_super_block super;
 	struct ext2_group_desc group;
 	int fd;
@@ -100,20 +103,23 @@ void Read_GroupDesc(){
 	read(fd, &group, sizeof(group));
 	close(fd);
 
-	printf("Reading first group-descriptor from device " FD_DEVICE ":\n"
-	       "Blocks bitmap block: %u\n"
-	       "Inodes bitmap block: %u\n"
-	       "Inodes table block : %u\n"
-	       "Free blocks count  : %u\n"
-	       "Free inodes count  : %u\n"
-	       "Directories count  : %u\n"
-	       ,
-	       group.bg_block_bitmap,
-	       group.bg_inode_bitmap,
-	       group.bg_inode_table,
-	       group.bg_free_blocks_count,
-	       group.bg_free_inodes_count,
-	       group.bg_used_dirs_count);    /* directories count */
+	if(print == 1){
+		printf("Reading first group-descriptor from device " FD_DEVICE ":\n"
+			"Blocks bitmap block: %u\n"
+			"Inodes bitmap block: %u\n"
+			"Inodes table block : %u\n"
+			"Free blocks count  : %u\n"
+			"Free inodes count  : %u\n"
+			"Directories count  : %u\n"
+			,
+			group.bg_block_bitmap,
+			group.bg_inode_bitmap,
+			group.bg_inode_table,
+			group.bg_free_blocks_count,
+			group.bg_free_inodes_count,
+			group.bg_used_dirs_count);    /* directories count */
+	}
+	return group;
 }
 
 void Read_RootInode(){
@@ -258,17 +264,103 @@ static void read_dir(int fd, const struct ext2_inode *inode, const struct ext2_g
 	}
 } /* read_dir() */
 
+static unsigned int Read_Inode_Number(int fd, const struct ext2_inode *inode, const struct ext2_group_desc *group, char* nome_arquivo)
+{
+	void *block;
+
+	if (S_ISDIR(inode->i_mode)) {
+		struct ext2_dir_entry_2 *entry;
+		unsigned int size = 0;
+
+		if ((block = malloc(block_size)) == NULL) { /* allocate memory for the data block */
+			fprintf(stderr, "Memory error\n");
+			close(fd);
+			exit(1);
+		}
+
+		lseek(fd, BLOCK_OFFSET(inode->i_block[0]), SEEK_SET);
+		read(fd, block, block_size);                /* read block from disk*/
+
+		entry = (struct ext2_dir_entry_2 *) block;  /* first entry in the directory */
+                /* Notice that the list may be terminated with a NULL
+                   entry (entry->inode == NULL)*/
+		while((size < inode->i_size) && entry->inode) {
+			char file_name[EXT2_NAME_LEN+1];
+			memcpy(file_name, entry->name, entry->name_len);
+			file_name[entry->name_len] = 0;     /* append null character to the file name */
+
+			if(strcmp(nome_arquivo, entry->name) == 0){
+				return entry->inode;
+			}
+
+			entry = (void*) entry + entry->rec_len;
+			size += entry->rec_len;
+		}
+
+		free(block);
+	}
+} /* retorna numero inode */
+
+void read_arq(int fd, struct ext2_inode *inode){
+	int size = inode->i_size;
+	char *bloco = malloc(block_size);
+
+	lseek(fd, BLOCK_OFFSET(inode->i_block[0]),SEEK_SET);
+	read(fd, bloco, block_size);
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%c", bloco[i]);
+	}
+	
+	
+}
+
+void Read_TXT(char* nome_arquivo){
+	struct ext2_super_block super;
+	struct ext2_group_desc group;
+	struct ext2_inode inode;
+	int fd;
+
+	/* open floppy device */
+
+	if ((fd = open(FD_DEVICE, O_RDONLY)) < 0) {
+		perror(FD_DEVICE);
+		exit(1);  /* error while opening the floppy device */
+	}
+
+	/* read super-block */
+
+	lseek(fd, BASE_OFFSET, SEEK_SET); 
+	read(fd, &super, sizeof(super));
+
+	if (super.s_magic != EXT2_SUPER_MAGIC) {
+		fprintf(stderr, "Not a Ext2 filesystem\n");
+		exit(1);
+	}
+		
+	block_size = 1024 << super.s_log_block_size;
+
+	/* read group descriptor */
+
+	lseek(fd, BASE_OFFSET + block_size, SEEK_SET);
+	read(fd, &group, sizeof(group));
+
+	/* show entries in the root directory */
+
+	read_inode(fd, 2, &group, &inode);   /* read inode 2 (root directory) */
+	unsigned int i_number = Read_Inode_Number(fd, &inode, &group, nome_arquivo);
+
+	printf("\n%d\n", i_number);
+	read_inode(fd, i_number, &group, &inode);
+
+	read_arq(fd, &inode);
+}// LS
+
+
 
 int main(){
 
-	// struct ext2_group_desc group;
-	// struct ext2_inode inode;
-    // int fd = open(FD_DEVICE, O_RDONLY);
-
-    // read_inode(fd, 2, &group, &inode);
-    // read_dir(fd, &inode, &group);
-
-	List_ETX();
 
     return 0;
 }
